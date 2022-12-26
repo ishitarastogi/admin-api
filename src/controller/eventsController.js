b8a8badda6460ac9159db25af48068306fc8bfb4
ishitarastogi <ishita3099@gmail.com>
2022-12-26 18:52:51 +0530

    deploy

--- src/controller/eventsController.js
+++ src/controller/eventsController.js
@@ -1,13 +1,14 @@
 const eventSchema = require("../models/event");
 const axios = require("axios");
-const ethers = require("ethers");
+
+
 async function addEvent(req, res) {
-  const wallet = req.body.wallet;
-  const eventName = req.body.eventName;
-  const eventHash = req.body.eventHash;
-  const provider = ethers.getDefaultProvider();
-  const lastBlockNumber = await provider.getBlockNumber();
+  const wallet = req.body.wallet; 
+  const eventName = req.body.eventName; 
+  const eventHash = req.body.eventHash; 
+  const lastBlockNumber = Number(req.body.lastBlockNumber); 
   const contractAddress = req.body.contractAddress;
+
   const eventObj = {
     wallet,
     eventName,
@@ -15,10 +16,12 @@ async function addEvent(req, res) {
     lastBlockNumber,
     contractAddress,
   };
+
   const eo = eventSchema(eventObj);
   const eos = await eo.save();
-  res.send(eos);
+  res.send(eos)
 }
+
 module.exports = {
   addEvent,
 };
